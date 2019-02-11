35580f4a6eeb530f7587b7708529f1ea81f406f6
Coos Wolff <coos.wolff@outlook.com>
2019-02-11 19:13:01 +0100

    Added method for deleting team member by name

--- src/main/java/com/wolffsoft/phonedestroyer/repository/TeamMemberRepository.java
+++ src/main/java/com/wolffsoft/phonedestroyer/repository/TeamMemberRepository.java
@@ -1,21 +1,26 @@
 package com.wolffsoft.phonedestroyer.repository;
 
 import com.wolffsoft.phonedestroyer.model.TeamMember;
+import com.wolffsoft.phonedestroyer.repository.mapper.OptionalTeamMemberMapper;
 import com.wolffsoft.phonedestroyer.repository.mapper.TeamMemberMapper;
 import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 import static com.wolffsoft.phonedestroyer.persistance.repositories.jooq.Tables.TEAM_MEMBER;
 
+@Repository
 public class TeamMemberRepository {
 
     private DSLContext dslContext;
+    private OptionalTeamMemberMapper optionalTeamMemberMapper;
     private TeamMemberMapper teamMemberMapper;
 
     public TeamMemberRepository(DSLContext dslContext) {
         this.dslContext = dslContext;
+        this.optionalTeamMemberMapper = new OptionalTeamMemberMapper();
         this.teamMemberMapper = new TeamMemberMapper();
     }
 
@@ -35,13 +40,20 @@ public class TeamMemberRepository {
                 .select()
                 .from(TEAM_MEMBER)
                 .where(TEAM_MEMBER.NAME.eq(name))
-                .fetchOne(teamMemberMapper);
+                .fetchOne(optionalTeamMemberMapper);
     }
 
     public List<TeamMember> getAllTeamMembers() {
         return dslContext
-                .insertInto(TEAM_MEMBER, TEAM_MEMBER.NAME, TEAM_MEMBER.DATE_JOINED)
-                .values(teamMember.getName(), teamMember.getJoinedTeam())
+                .select()
+                .from(TEAM_MEMBER)
+                .fetch(teamMemberMapper);
+    }
+
+    public void deleteTeamMemberByName(String name) {
+        dslContext
+                .deleteFrom(TEAM_MEMBER)
+                .where(TEAM_MEMBER.NAME.eq(name))
                 .execute();
     }
 
