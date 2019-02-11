febfbfe2be4567124916bbf432d73cdcbba9f7de
Coos Wolff <coos.wolff@outlook.com>
2019-02-11 19:12:31 +0100

    Added mapper for non optional Team member and renamed mapper for optional Team member

--- src/main/java/com/wolffsoft/phonedestroyer/repository/mapper/OptionalTeamMemberMapper.java
+++ src/main/java/com/wolffsoft/phonedestroyer/repository/mapper/OptionalTeamMemberMapper.java
@@ -4,16 +4,18 @@ import com.wolffsoft.phonedestroyer.model.TeamMember;
 import org.jooq.Record;
 import org.jooq.RecordMapper;
 
+import java.util.Optional;
+
 import static com.wolffsoft.phonedestroyer.persistance.repositories.jooq.Tables.TEAM_MEMBER;
 
-public class TeamMemberMapper implements RecordMapper<Record, TeamMember> {
+public class OptionalTeamMemberMapper implements RecordMapper<Record, Optional<TeamMember>> {
 
     @Override
-    public TeamMember map(Record record) {
-        return TeamMember.builder()
+    public Optional<TeamMember> map(Record record) {
+        return Optional.of(TeamMember.builder()
                 .id(record.get(TEAM_MEMBER.ID))
                 .name(record.get(TEAM_MEMBER.NAME))
                 .joinedTeam(record.get(TEAM_MEMBER.DATE_JOINED))
-                .build();
+                .build());
     }
 }
