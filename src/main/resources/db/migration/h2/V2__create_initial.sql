6248bd69a58793c3930a03a257f38314f06a7cd6
Coos Wolff <coos.wolff@outlook.com>
2019-02-11 19:10:18 +0100

    Added comment, dropping of tables and formatted

--- src/main/resources/db/migration/h2/V2__create_initial.sql
+++ src/main/resources/db/migration/h2/V2__create_initial.sql
@@ -1,22 +1,29 @@
+-- TODO REMOVE DROPPING TABLES IN PRODUCTION !!!!
 DROP TABLE IF EXISTS event;
-CREATE TABLE event (
-  id INT NOT NULL AUTO_INCREMENT ,
-  event_date VARCHAR(30) NOT NULL ,
-  name VARCHAR (255) NOT NULL ,
+CREATE TABLE event
+(
+  id             INT          NOT NULL AUTO_INCREMENT,
+  team_member_id INT(30)      NULL,
+  event_date     VARCHAR(30)  NOT NULL,
+  name           VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
+-- TODO REMOVE DROPPING TABLES IN PRODUCTION !!!!
 DROP TABLE IF EXISTS team;
-CREATE TABLE team (
-  id INT NOT NULL AUTO_INCREMENT ,
-  team_member VARCHAR (255) NOT NULL ,
+CREATE TABLE team
+(
+  id          INT          NOT NULL AUTO_INCREMENT,
+  team_member VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
+-- TODO REMOVE DROPPING TABLES IN PRODUCTION !!!!
 DROP TABLE IF EXISTS team_member;
-CREATE TABLE team_member (
-  id INT NOT NULL AUTO_INCREMENT ,
-  name VARCHAR (255) ,
-  date_joined VARCHAR (30) NOT NULL ,
+CREATE TABLE team_member
+(
+  id          INT         NOT NULL AUTO_INCREMENT,
+  name        VARCHAR(255),
+  date_joined VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
