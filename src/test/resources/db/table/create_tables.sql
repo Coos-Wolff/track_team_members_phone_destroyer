263d56b24dedad4f2ec9e488212dc60bda8ebe6b
Coos Wolff <coos.wolff@outlook.com>
2019-02-11 19:22:27 +0100

    Added column to event table

--- src/test/resources/db/table/create_tables.sql
+++ src/test/resources/db/table/create_tables.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS event;
 CREATE TABLE event (
   id INT NOT NULL AUTO_INCREMENT ,
+  team_member_id INT (30) NULL ,
   event_date VARCHAR (30) NOT NULL ,
   name VARCHAR (255) NOT NULL ,
   PRIMARY KEY (id)
