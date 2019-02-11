c331155e6bb1c632051100dc95160dada59ccae8
Coos Wolff <coos.wolff@outlook.com>
2019-02-11 19:10:35 +0100

    Added tests

--- src/test/java/com/wolffsoft/phonedestroyer/repository/TeamMemberRepositoryTest.java
+++ src/test/java/com/wolffsoft/phonedestroyer/repository/TeamMemberRepositoryTest.java
@@ -8,14 +8,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TeamMemberRepositoryTest extends AbstractTestRepository<TeamMemberRepository> {
 
-    TeamMember teamMember1;
+    private TeamMember teamMember3;
+    private TeamMember teamMember4;
 
     @Override
     protected TeamMemberRepository createRepository(DSLContext dslContext) {
@@ -24,18 +28,37 @@ public class TeamMemberRepositoryTest extends AbstractTestRepository<TeamMemberR
 
     @Before
     public void setup() {
-        teamMember1 = TeamMember.builder()
-                .id(1)
-                .name("Team Member 1")
+        teamMember3 = TeamMember.builder()
+                .id(3)
+                .name("Team Member 3")
+                .joinedTeam("01-01-2019")
+                .build();
+
+        teamMember4 = TeamMember.builder()
+                .id(4)
+                .name("Team Member 4")
                 .joinedTeam("01-01-2019")
                 .build();
     }
 
     @Test
     public void testAddMember() {
-        int addedTeamMember = repository.addTeamMember(teamMember1);
+        int addedTeamMember = repository.addTeamMember(teamMember3);
 
         assertThat(addedTeamMember).isEqualTo(1);
+    }
+
+    @Test
+    public void testAddMembers() {
+        List<TeamMember> teamMembers = Stream.of(teamMember3, teamMember4).collect(Collectors.toList());
+
+        repository.addTeamMembers(teamMembers);
+
+        Optional<TeamMember> addedTeamMember1 = repository.getTeamMemberByName(teamMember3.getName());
+        Optional<TeamMember> addedTeamMember2 = repository.getTeamMemberByName(teamMember4.getName());
+
+        assertThat(addedTeamMember1.get().getName()).isEqualTo(teamMember3.getName());
+        assertThat(addedTeamMember2.get().getName()).isEqualTo(teamMember4.getName());
     }
 
     @Test
@@ -44,6 +67,34 @@ public class TeamMemberRepositoryTest extends AbstractTestRepository<TeamMemberR
 
         Optional<TeamMember> returnedTeamMember = repository.getTeamMemberByName(name);
 
-        assertThat(returnedTeamMember).isEqualTo(Optional.of(teamMember1));
+        assertThat(returnedTeamMember).isEqualTo(Optional.of(teamMember3));
+    }
+
+    @Test
+    public void testDeleteTeamMember() {
+        int addedTeamMember = repository.addTeamMember(teamMember3);
+
+        assertThat(addedTeamMember).isEqualTo(1);
+
+        String name = teamMember3.getName();
+        repository.deleteTeamMemberByName(name);
+
+        Optional<TeamMember> deletedTeamMember = repository.getTeamMemberByName(name);
+        assertThat(deletedTeamMember).isNull();
+    }
+
+    @Test
+    public void testDeleteTeamMembers() {
+        List<TeamMember> teamMembers = Stream.of(teamMember3, teamMember4).collect(Collectors.toList());
+
+        repository.addTeamMembers(teamMembers);
+        List<TeamMember> allTeamMembers = repository.getAllTeamMembers();
+
+        assertThat(allTeamMembers.size()).isEqualTo(4);
+
+        repository.deleteTeamMembersByName(teamMembers);
+
+        allTeamMembers = repository.getAllTeamMembers();
+        assertThat(allTeamMembers.size()).isEqualTo(2);
     }
 }
