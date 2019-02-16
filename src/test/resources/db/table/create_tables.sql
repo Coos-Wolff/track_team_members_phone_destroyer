b45b98630bc78dd8b8136839346e479286ed8535
Coos Wolff <coos.wolff@outlook.com>
2019-02-16 22:53:46 +0100

    Added new column to the team_member table to store the amount of tickets for the current event

--- src/test/resources/db/table/create_tables.sql
+++ src/test/resources/db/table/create_tables.sql
@@ -12,6 +12,7 @@ CREATE TABLE team_member (
   id INT NOT NULL AUTO_INCREMENT ,
   name VARCHAR (255) ,
   date_joined VARCHAR (30) NOT NULL ,
+  tickets_collected_current_event INT(6)      NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
