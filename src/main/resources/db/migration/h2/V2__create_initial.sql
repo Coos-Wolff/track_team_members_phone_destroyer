bcdc40b7a5084d2b20391e1e877d479c4da35d67
Coos Wolff <coos.wolff@outlook.com>
2019-02-16 22:46:46 +0100

    Added new fields to tables and formatted the file

--- src/main/resources/db/migration/h2/V2__create_initial.sql
+++ src/main/resources/db/migration/h2/V2__create_initial.sql
@@ -1,23 +1,25 @@
 CREATE TABLE IF NOT EXISTS event
 (
   id             INT          NOT NULL AUTO_INCREMENT,
-  team_member_id INT(30)      NULL,
   event_date     VARCHAR(30)  NOT NULL,
   name           VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS team
+CREATE TABLE IF NOT EXISTS team_member
 (
-  id          INT          NOT NULL AUTO_INCREMENT,
-  team_member VARCHAR(255) NOT NULL,
+  id                              INT         NOT NULL AUTO_INCREMENT,
+  name                            VARCHAR(255),
+  date_joined                     VARCHAR(30) NOT NULL,
+  tickets_collected_current_event INT(6)      NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS team_member
+CREATE TABLE IF NOT EXISTS event_team_member
 (
-  id          INT         NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(255),
-  date_joined VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  event_id       INT(30) NOT NULL,
+  team_member_id INT(30) NOT NULL,
+  FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (team_member_id) REFERENCES team_member (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (event_id, team_member_id)
 );
