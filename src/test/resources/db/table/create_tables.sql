63946081e1d3deb79ae885bc261c22d8ecef8a70
Coos Wolff <coos.wolff@outlook.com>
2019-02-16 22:54:00 +0100

    Formatted

--- src/test/resources/db/table/create_tables.sql
+++ src/test/resources/db/table/create_tables.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS event;
-CREATE TABLE event (
-  id INT NOT NULL AUTO_INCREMENT ,
-  team_member_id INT (30) NULL ,
-  event_date VARCHAR (30) NOT NULL ,
-  name VARCHAR (255) NOT NULL ,
+CREATE TABLE event
+(
+  id             INT          NOT NULL AUTO_INCREMENT,
+  team_member_id INT(30)      NULL,
+  event_date     VARCHAR(30)  NOT NULL,
+  name           VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS team_member;
-CREATE TABLE team_member (
-  id INT NOT NULL AUTO_INCREMENT ,
-  name VARCHAR (255) ,
-  date_joined VARCHAR (30) NOT NULL ,
+CREATE TABLE team_member
+(
+  id                              INT         NOT NULL AUTO_INCREMENT,
+  name                            VARCHAR(255),
+  date_joined                     VARCHAR(30) NOT NULL,
   tickets_collected_current_event INT(6)      NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
