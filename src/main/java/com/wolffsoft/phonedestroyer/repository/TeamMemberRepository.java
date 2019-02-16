571ef96386f5db40af017a20e0891096e28d0d06
Coos Wolff <coos.wolff@outlook.com>
2019-02-16 22:44:38 +0100

    Added method for retrieving team members by event name.

--- src/main/java/com/wolffsoft/phonedestroyer/repository/TeamMemberRepository.java
+++ src/main/java/com/wolffsoft/phonedestroyer/repository/TeamMemberRepository.java
@@ -60,4 +60,17 @@ public class TeamMemberRepository {
     public void deleteTeamMembersByName(List<TeamMember> teamMembers) {
         teamMembers.forEach(teamMember -> deleteTeamMemberByName(teamMember.getName()));
     }
+
+    private List<TeamMember> getTeamMembersByEventName(String eventName) {
+        return dslContext
+                .select()
+                .from(TEAM_MEMBER)
+                .join(EVENT_TEAM_MEMBER)
+                .on(TEAM_MEMBER.ID.eq(EVENT_TEAM_MEMBER.TEAM_MEMBER_ID))
+                .join(EVENT)
+                .on(EVENT.ID.eq(EVENT_TEAM_MEMBER.EVENT_ID))
+                .where(EVENT.NAME.eq(eventName))
+                .orderBy(TEAM_MEMBER.ID)
+                .fetch(teamMemberMapper);
+    }
 }
