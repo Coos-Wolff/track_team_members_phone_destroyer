58f2bb9674606d266e1b164bfbaac23e77285da5
Coos Wolff <coos.wolff@outlook.com>
2019-02-17 22:46:26 +0100

    Added mappers to the constructor used in the repository

--- src/main/java/com/wolffsoft/phonedestroyer/repository/EventRepository.java
+++ src/main/java/com/wolffsoft/phonedestroyer/repository/EventRepository.java
@@ -1,7 +1,10 @@
 package com.wolffsoft.phonedestroyer.repository;
 
+import com.wolffsoft.phonedestroyer.model.CreateEvent;
 import com.wolffsoft.phonedestroyer.model.Event;
 import com.wolffsoft.phonedestroyer.repository.mapper.EventMapper;
+import com.wolffsoft.phonedestroyer.repository.mapper.OptionalEventMapper;
+import com.wolffsoft.phonedestroyer.repository.mapper.TeamMemberMapper;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
@@ -14,10 +17,16 @@ public class EventRepository {
 
     private DSLContext dslContext;
     private EventMapper eventMapper;
+    private OptionalEventMapper optionalEventMapper;
+    private TeamMemberMapper teamMemberMapper;
 
     public EventRepository(DSLContext dslContext) {
         this.dslContext = dslContext;
         this.eventMapper = new EventMapper();
+        this.teamMemberMapper = new TeamMemberMapper();
+        this.optionalEventMapper = new OptionalEventMapper();
+    }
+
     public void createNewEvent(CreateEvent createEvent) {
         dslContext
                 .insertInto(EVENT, EVENT.NAME, EVENT.EVENT_DATE)
