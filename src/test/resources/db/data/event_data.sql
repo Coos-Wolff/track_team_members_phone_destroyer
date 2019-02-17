fc75de6d35146b10a238b46dc6eed0e85d67f77a
Coos Wolff <coos.wolff@outlook.com>
2019-02-17 18:26:03 +0100

    Added test data

--- src/test/resources/db/data/event_data.sql
+++ src/test/resources/db/data/event_data.sql
@@ -1,2 +1,7 @@
 INSERT INTO event (id, event_date, `name`)
-VALUES (1, '10-02-2019' , 'Test Event 1');
+VALUES (1, '10-02-2019', 'Test Event 1'),
+       (2, '11-03-2019', 'Test Event 2'),
+       (3, '12-04-2019', 'Test Event 3'),
+       (4, '13-05-2019', 'Test Event 4'),
+       (5, '14-06-2019', 'Test Event 5'),
+       (6, '15-07-2019', 'Test Event 6');
