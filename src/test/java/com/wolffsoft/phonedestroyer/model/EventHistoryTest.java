059c89bbb86755824d1dfc42dd2cd91d7f57cfa0
Coos Wolff <coos.wolff@outlook.com>
2019-02-17 21:29:29 +0100

    Created new class to hold Event History and created corresponding test class

---
+++ src/test/java/com/wolffsoft/phonedestroyer/model/EventHistoryTest.java
@@ -0,0 +1,38 @@
+package com.wolffsoft.phonedestroyer.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventHistoryTest {
+
+    private static final String EVENT_NAME = "Test Event Name";
+    private static final int TEAM_MEMBER_ID = 1;
+    private static final int EVENT_ID = 1;
+    private static final int EVENT_TICKETS_COLLECTED = 550;
+
+    private EventHistory testEventHistory;
+
+    @Before
+    public void setup() {
+        testEventHistory = EventHistory.builder()
+                .eventName(EVENT_NAME)
+                .eventId(EVENT_ID)
+                .teamMemberId(TEAM_MEMBER_ID)
+                .eventTicketsCollected(EVENT_TICKETS_COLLECTED)
+                .build();
+    }
+
+    @Test
+    public void testEventHistory() {
+        EventHistory eventHistory = EventHistory.builder()
+                .eventName(EVENT_NAME)
+                .eventId(EVENT_ID)
+                .teamMemberId(TEAM_MEMBER_ID)
+                .eventTicketsCollected(EVENT_TICKETS_COLLECTED)
+                .build();
+
+        assertThat(eventHistory).isEqualTo(testEventHistory);
+    }
+}
