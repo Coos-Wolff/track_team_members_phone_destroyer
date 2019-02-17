3768d60966fb641fe28657f062d46b6891cc26ec
Coos Wolff <coos.wolff@outlook.com>
2019-02-17 21:27:16 +0100

    Refactored class name and refactored method by splitting the method into 2 methods. This is done to gain more flexibility.

--- src/main/java/com/wolffsoft/phonedestroyer/model/InstantNow.java
+++ src/main/java/com/wolffsoft/phonedestroyer/model/InstantNow.java
@@ -5,13 +5,17 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-public class InstantNowToString {
+public class InstantNow {
 
-    public static String instantToString() {
+    public static Instant instantNow() {
+        return Instant.now();
+    }
+
+    public static String formatInstantToString(Instant instant) {
         DateTimeFormatter formatter = DateTimeFormatter
                 .ofLocalizedDate(FormatStyle.SHORT)
                 .withZone(ZoneId.systemDefault());
 
-        return formatter.format(Instant.now());
+        return formatter.format(instant);
     }
 }
