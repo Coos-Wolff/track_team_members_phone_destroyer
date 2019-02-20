2224b9ba8d3e6b06c2364810a1e690a0404c9c18
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 14:25:13 +0100

    Added test data to for event because event has a new column has_ended.

--- src/test/resources/db/data/event_data.sql
+++ src/test/resources/db/data/event_data.sql
@@ -1,8 +1,8 @@
-INSERT INTO event (id, event_date, `name`)
-VALUES (1, '10-02-2019', 'Test Event 1'),
-       (2, '11-03-2019', 'Test Event 2'),
-       (3, '12-04-2019', 'Test Event 3'),
-       (4, '13-05-2019', 'Test Event 4'),
-       (5, '14-06-2019', 'Test Event 5'),
-       (6, '15-07-2019', 'Test Event 6'),
-       (123456789, '01-01-1970', 'Test Event 123456789');
+INSERT INTO event (id, event_date, `name`, has_ended)
+VALUES (1, '10-02-2019', 'Test Event 1', false),
+       (2, '11-03-2019', 'Test Event 2', false),
+       (3, '12-04-2019', 'Test Event 3', true),
+       (4, '13-05-2019', 'Test Event 4', true),
+       (5, '14-06-2019', 'Test Event 5', false),
+       (6, '15-07-2019', 'Test Event 6', false),
+       (123456789, '01-01-1970', 'Test Event 123456789', true);
