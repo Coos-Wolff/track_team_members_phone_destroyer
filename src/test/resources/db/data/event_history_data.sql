c77ed459aa97a4a92d03d5cdf2bb229ed5e38063
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 18:55:57 +0100

    Added extra test data.

--- src/test/resources/db/data/event_history_data.sql
+++ src/test/resources/db/data/event_history_data.sql
@@ -1,4 +1,11 @@
 INSERT INTO event_history(id, event_name, member_name, member_id, event_id, event_tickets_collected)
 VALUES (1, 'Test Event 1', 'Member 1', 1, 1, 150),
-       (2, 'Test Event 2', 'Member 1',  1, 2, 1543),
-       (3, 'Test Event 1', 'Member 2', 2, 1, 443);
+       (2, 'Test Event 1', 'Member 2',  1, 2, 1543),
+       (3, 'Test Event 1', 'Member 3', 2, 1, 443),
+       (4, 'Test Event 2', 'Member 1', 2, 1, 150),
+       (5, 'Test Event 2', 'Member 2', 2, 1, 1234),
+       (6, 'Test Event 3', 'Member 1', 2, 1, 156),
+       (7, 'Test Event 3', 'Member 2', 2, 1, 702),
+       (8, 'Test Event 4', 'Member 1', 2, 1, 1111),
+       (9, 'Test Event 4', 'Member 2', 2, 1, 800),
+       (10, 'Test Event 4', 'Member 3', 2, 1, 925);
