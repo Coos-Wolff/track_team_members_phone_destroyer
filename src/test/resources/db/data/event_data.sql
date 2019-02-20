eedd66c855adb8957bc4b24c3b8e04f15fcd42fe
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 18:55:47 +0100

    Fixed spelling.

--- src/test/resources/db/data/event_data.sql
+++ src/test/resources/db/data/event_data.sql
@@ -4,5 +4,5 @@ VALUES (1, '10-02-2019', 'Test Event 1', false, 'Points'),
        (3, '12-04-2019', 'Test Event 3', true, 'Tickets'),
        (4, '13-05-2019', 'Test Event 4', true, 'Tickets'),
        (5, '14-06-2019', 'Test Event 5', false, 'Points'),
-       (6, '15-07-2019', 'Test Event 6', false, 'Tikcets'),
+       (6, '15-07-2019', 'Test Event 6', false, 'Tickets'),
        (123456789, '01-01-1970', 'Test Event 123456789', false, 'Tickets');
