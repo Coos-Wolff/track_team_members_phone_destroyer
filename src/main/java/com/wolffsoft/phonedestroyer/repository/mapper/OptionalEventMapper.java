e5814d6942651f7d0ef705b2098d452a5b7a3f54
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 14:26:05 +0100

    Adjusted mapper to map all the column from the database to the Event object

--- src/main/java/com/wolffsoft/phonedestroyer/repository/mapper/OptionalEventMapper.java
+++ src/main/java/com/wolffsoft/phonedestroyer/repository/mapper/OptionalEventMapper.java
@@ -13,7 +13,10 @@ public class OptionalEventMapper implements RecordMapper<Record, Optional<Event>
     @Override
     public Optional<Event> map(Record record) {
         return Optional.of(Event.builder()
+                .id(record.get(EVENT.ID))
                 .name(record.get(EVENT.NAME))
+                .eventDate(record.get(EVENT.EVENT_DATE))
+                .eventHasEnded(record.get(EVENT.HAS_ENDED))
                 .build());
     }
 }
