3f098fb52e1e29e949d07585263dfd93d14d86ba
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 13:43:43 +0100

    Removed unused mappers from constructor.

--- src/main/java/com/wolffsoft/phonedestroyer/repository/EventRepository.java
+++ src/main/java/com/wolffsoft/phonedestroyer/repository/EventRepository.java
@@ -2,9 +2,7 @@ package com.wolffsoft.phonedestroyer.repository;
 
 import com.wolffsoft.phonedestroyer.model.CreateEvent;
 import com.wolffsoft.phonedestroyer.model.Event;
-import com.wolffsoft.phonedestroyer.repository.mapper.EventMapper;
 import com.wolffsoft.phonedestroyer.repository.mapper.OptionalEventMapper;
-import com.wolffsoft.phonedestroyer.repository.mapper.MemberMapper;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
@@ -16,14 +14,10 @@ import static com.wolffsoft.phonedestroyer.persistance.repositories.jooq.Tables.
 public class EventRepository {
 
     private DSLContext dslContext;
-    private EventMapper eventMapper;
     private OptionalEventMapper optionalEventMapper;
-    private MemberMapper memberMapper;
 
     public EventRepository(DSLContext dslContext) {
         this.dslContext = dslContext;
-        this.eventMapper = new EventMapper();
-        this.memberMapper = new MemberMapper();
         this.optionalEventMapper = new OptionalEventMapper();
     }
 
