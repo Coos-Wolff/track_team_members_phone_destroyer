ea3c2b3a3b993333f6d053978133ab55f86d4b7b
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 19:24:14 +0100

    Changed test data values.

--- src/test/resources/db/data/event_data.sql
+++ src/test/resources/db/data/event_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO event (id, event_date, `name`, has_ended, event_type)
-VALUES (1, '10-02-2019', 'Test Event 1', false, 'Points'),
-       (2, '11-03-2019', 'Test Event 2', false, 'Points'),
+VALUES (1, '10-02-2019', 'Test Event 1', true, 'Points'),
+       (2, '11-03-2019', 'Test Event 2', true, 'Points'),
        (3, '12-04-2019', 'Test Event 3', true, 'Tickets'),
        (4, '13-05-2019', 'Test Event 4', true, 'Tickets'),
-       (5, '14-06-2019', 'Test Event 5', false, 'Points'),
-       (6, '15-07-2019', 'Test Event 6', false, 'Tickets'),
+       (5, '14-06-2019', 'Test Event 5', true, 'Points'),
+       (6, '15-07-2019', 'Test Event 6', true, 'Tickets'),
        (123456789, '01-01-1970', 'Test Event 123456789', false, 'Tickets');
