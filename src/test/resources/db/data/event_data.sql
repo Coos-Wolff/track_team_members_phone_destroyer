16dc1f316376feef7bb6d787565beb7140fd28b6
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 16:03:04 +0100

    Changed value.

--- src/test/resources/db/data/event_data.sql
+++ src/test/resources/db/data/event_data.sql
@@ -5,4 +5,4 @@ VALUES (1, '10-02-2019', 'Test Event 1', false),
        (4, '13-05-2019', 'Test Event 4', true),
        (5, '14-06-2019', 'Test Event 5', false),
        (6, '15-07-2019', 'Test Event 6', false),
-       (123456789, '01-01-1970', 'Test Event 123456789', true);
+       (123456789, '01-01-1970', 'Test Event 123456789', false);
