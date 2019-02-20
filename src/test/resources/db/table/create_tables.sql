a4e64a50f2c5b40e58f436a9768983f4e6fad04a
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 14:26:47 +0100

    Formatted

--- src/test/resources/db/table/create_tables.sql
+++ src/test/resources/db/table/create_tables.sql
@@ -22,7 +22,7 @@ CREATE TABLE member
 DROP TABLE IF EXISTS event_member;
 CREATE TABLE event_member
 (
-  event_id       INT(30) NOT NULL,
+  event_id  INT(30) NOT NULL,
   member_id INT(30) NOT NULL,
   FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE RESTRICT ON UPDATE CASCADE,
   FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS event_history;
 CREATE TABLE event_history
 (
   id                      INT          NOT NULL AUTO_INCREMENT,
-  member_id          INT(30)      NOT NULL,
+  member_id               INT(30)      NOT NULL,
   event_id                INT(20)      NOT NULL,
   event_tickets_collected INT(6),
   FOREIGN KEY (member_id) REFERENCES member (id),
