3f3115bd55215b34f8b9003e1bb2fa7e73abf53e
Coos Wolff <coos.wolff@outlook.com>
2019-02-20 13:50:37 +0100

    Added new field to the Event object to set if an event is ended or not. These are the corresponding tests.

--- src/test/java/com/wolffsoft/phonedestroyer/model/EventTest.java
+++ src/test/java/com/wolffsoft/phonedestroyer/model/EventTest.java
@@ -3,27 +3,56 @@ package com.wolffsoft.phonedestroyer.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static com.wolffsoft.phonedestroyer.helperclass.model.MemberTestObject.getTestTeamMembers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EventTest {
 
     private static final String EVENT_NAME = "TestEvent";
-
-    private Event testEvent;
+    private static final List<Member> testMembers = getTestTeamMembers();
+    private Event testCurrentEvent;
+    private Event testEndedEvent;
 
     @Before
     public void setup() {
-        testEvent = Event.builder()
-                .name("TestEvent")
+        testCurrentEvent = Event.builder()
+                .id(1)
+                .name(EVENT_NAME)
+                .members(testMembers)
+                .eventHasEnded(false)
+                .build();
+
+        testEndedEvent = Event.builder()
+                .id(1)
+                .members(testMembers)
+                .name(EVENT_NAME)
+                .eventHasEnded(true)
                 .build();
     }
 
     @Test
-    public void testEvent() {
+    public void testCurrentEvent() {
         Event event = Event.builder()
+                .id(1)
                 .name(EVENT_NAME)
+                .members(testMembers)
+                .eventHasEnded(false)
                 .build();
 
-        assertThat(event).isEqualTo(testEvent);
+        assertThat(event).isEqualTo(testCurrentEvent);
+    }
+
+    @Test
+    public void testEndedEvent() {
+        Event event = Event.builder()
+                .id(1)
+                .name(EVENT_NAME)
+                .members(testMembers)
+                .eventHasEnded(true)
+                .build();
+
+        assertThat(event).isEqualTo(testEndedEvent);
     }
 }
