68a995199d03f12b96223bd9c2fa93ec55e71fb6
Coos Wolff <coos.wolff@outlook.com>
2019-03-03 00:14:48 +0100

    Moved calculation of members to be kicked to its own class

--- src/main/java/com/wolffsoft/phonedestroyer/service/EventService.java
+++ src/main/java/com/wolffsoft/phonedestroyer/service/EventService.java
@@ -1,6 +1,9 @@
 package com.wolffsoft.phonedestroyer.service;
 
-import com.wolffsoft.phonedestroyer.model.*;
+import com.wolffsoft.phonedestroyer.model.CreateEvent;
+import com.wolffsoft.phonedestroyer.model.Event;
+import com.wolffsoft.phonedestroyer.model.EventTicket;
+import com.wolffsoft.phonedestroyer.model.Member;
 import com.wolffsoft.phonedestroyer.repository.EventHistoryRepository;
 import com.wolffsoft.phonedestroyer.repository.EventMemberRepository;
 import com.wolffsoft.phonedestroyer.repository.EventRepository;
@@ -9,10 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 public class EventService {
@@ -72,32 +72,8 @@ public class EventService {
         eventRepository.endEvent(event);
         List<Member> members = memberRepository.getAllMembers();
         members.forEach(member -> {
-            memberRepository.setTicketsToZero(member);
+            memberRepository.setTicketsAndPointsToZero(member);
             eventHistoryRepository.storeEventHistory(member, event);
         });
-    }
-
-    public List<Member> calculateMembersToBeKicked() {
-        List<Member> membersToBeKicked = new ArrayList<>();
-        List<String> memberNames = new ArrayList<>();
-        List<Event> lastTwoEvents = eventRepository.getLastTwoEvents();
-        String eventName1 = lastTwoEvents.get(0).getName();
-        String eventName2 = lastTwoEvents.get(1).getName();
-
-        List<EventHistory> eventHistoryLastTwoEvents = eventHistoryRepository
-                .getEventHistoryOfLastTwoEvents(eventName1, eventName2);
-
-        eventHistoryLastTwoEvents.removeIf(eventHistory -> eventHistory.getEventTicketsCollected() > 50);
-
-        eventHistoryLastTwoEvents.forEach(eventHistory -> memberNames.add(eventHistory.getMemberName()));
-        memberNames.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream()
-                .filter(name -> name.getValue() > 1L)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList())
-                .forEach(name -> memberRepository.getMemberByName(name).ifPresent(membersToBeKicked::add));
-
-        return membersToBeKicked;
     }
 }
