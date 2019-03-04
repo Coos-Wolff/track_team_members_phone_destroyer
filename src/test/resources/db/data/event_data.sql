5ef9fb883f21d6ca5f41f7b76bb6f84aad3449ce
Coos Wolff <coos.wolff@outlook.com>
2019-03-04 22:10:29 +0100

    Refactored date because local date can't parse it otherwise

--- src/test/resources/db/data/event_data.sql
+++ src/test/resources/db/data/event_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO event (id, event_date, `name`, has_ended, event_type)
-VALUES (1, '10-02-2019', 'Test Event 1', true, 'Points'),
-       (2, '11-03-2019', 'Test Event 2', true, 'Points'),
-       (3, '12-04-2019', 'Test Event 3', true, 'Tickets'),
-       (4, '13-05-2019', 'Test Event 4', true, 'Tickets'),
-       (5, '14-06-2019', 'Test Event 5', true, 'Points'),
-       (6, '15-07-2019', 'Test Event 6', true, 'Tickets'),
-       (123456789, '01-01-1970', 'Test Event 123456789', false, 'Tickets');
+VALUES (1, '2019-02-10', 'Test Event 1', true, 'Points'),
+       (2, '2019-03-11', 'Test Event 2', true, 'Points'),
+       (3, '2019-04-12', 'Test Event 3', true, 'Tickets'),
+       (4, '2019-05-13', 'Test Event 4', true, 'Tickets'),
+       (5, '2019-06-14', 'Test Event 5', true, 'Points'),
+       (6, '2019-07-15', 'Test Event 6', true, 'Tickets'),
+       (123456789, '1970-01-01', 'Test Event 123456789', false, 'Tickets');
