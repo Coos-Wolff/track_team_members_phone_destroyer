5f1c73c5730849c66ca737bc59949c5bdaa663df
Coos Wolff <coos.wolff@outlook.com>
2019-03-04 22:18:30 +0100

    Refactored the calculation method to take elders in to account

--- src/main/java/com/wolffsoft/phonedestroyer/calculate/Calculate.java
+++ src/main/java/com/wolffsoft/phonedestroyer/calculate/Calculate.java
@@ -1,55 +1,124 @@
 package com.wolffsoft.phonedestroyer.calculate;
 
-import com.wolffsoft.phonedestroyer.model.Event;
 import com.wolffsoft.phonedestroyer.model.EventHistory;
 import com.wolffsoft.phonedestroyer.repository.EventHistoryRepository;
-import com.wolffsoft.phonedestroyer.repository.EventRepository;
 
-import java.util.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Calculate {
 
-    private EventRepository eventRepository;
+    private static final LocalDate ONE_WEEK_AGO = LocalDate.now().minusWeeks(1);
+    private static final LocalDate TWO_WEEKS_AGO = LocalDate.now().minusWeeks(2);
+    private static final LocalDate FOUR_WEEKS_AGO = LocalDate.now().minusWeeks(4);
     private EventHistoryRepository eventHistoryRepository;
 
-    public Calculate(EventRepository eventRepository, EventHistoryRepository eventHistoryRepository) {
-        this.eventRepository = eventRepository;
+    public Calculate(EventHistoryRepository eventHistoryRepository) {
         this.eventHistoryRepository = eventHistoryRepository;
     }
 
-    public List<String> calculateMembersToBeKicked() {
-        List<String> memberNames = new ArrayList<>();
-        List<Event> lastTwoEvents = getLastTwoEvents();
-        List<EventHistory> eventHistoryLastTwoEvents = getEventHistoryLastTwoEvents(lastTwoEvents);
-        eventHistoryLastTwoEvents.removeIf(eventHistory ->
-                (eventHistory.getEventTicketsCollected() > 50 && eventHistory.getPointsCollected() == 0) ||
-                        (eventHistory.getEventTicketsCollected() == 0 && eventHistory.getPointsCollected() > 10)
-        );
+    public List<String> membersToBeKicked() {
+        List<EventHistory> eventHistoryLastFourEvents = getEventHistoryLastFourEvents();
+        List<String> membersToKick = new ArrayList<>();
+        List<EventHistory> eventHistoryOfMembers = new ArrayList<>();
+        List<EventHistory> eventHistoryOfElders = new ArrayList<>();
 
-        eventHistoryLastTwoEvents.forEach(eventHistory -> memberNames.add(eventHistory.getMemberName()));
+        divideMembersIntoSeparateLists(eventHistoryLastFourEvents, eventHistoryOfElders, eventHistoryOfMembers);
+        calculateMembersToKick(eventHistoryOfMembers).forEach(membersToKick::add);
+        calculateEldersToKick(eventHistoryOfElders).forEach(membersToKick::add);
 
-        return memberNames.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        return membersToKick;
+    }
+
+    public List<EventHistory> getEventHistoryLastFourEvents() {
+        List<EventHistory> eventHistories = eventHistoryRepository.getEventHistories();
+        eventHistories.removeIf(eventHistory -> eventHistory.getEventDate().isBefore(FOUR_WEEKS_AGO));
+
+        return eventHistories;
+    }
+
+    private void divideMembersIntoSeparateLists(
+            List<EventHistory> eventHistoryLastFourEvents,
+            List<EventHistory> eventHistoryOfElders,
+            List<EventHistory> eventHistoryOfMembers) {
+        eventHistoryLastFourEvents.forEach(eventHistory -> {
+            if (eventHistory.getRole().equalsIgnoreCase("Elder")) {
+                eventHistoryOfElders.add(eventHistory);
+            } else {
+                eventHistoryOfMembers.add(eventHistory);
+            }
+        });
+    }
+
+    private List<String> calculateMembersToKick(List<EventHistory> eventHistoryOfMembers) {
+        List<EventHistory> beginnersThreshold = new ArrayList<>();
+        List<EventHistory> membersThreshold = new ArrayList<>();
+        List<String> membersToKick = new ArrayList<>();
+
+        eventHistoryOfMembers.removeIf(eventHistory -> eventHistory.getEventDate().isBefore(TWO_WEEKS_AGO));
+        eventHistoryOfMembers.forEach(eventHistory -> {
+            if (thresholdBeginner(eventHistory)) {
+                beginnersThreshold.add(eventHistory);
+            } else if (thresholdPoints(eventHistory) || thresholdTickets(eventHistory)) {
+                membersThreshold.add(eventHistory);
+            }
+        });
+        beginnersThreshold.forEach(eventHistory -> membersToKick.add(eventHistory.getMemberName()));
+        getDuplicates(membersThreshold).forEach(eventHistory -> membersToKick.add(eventHistory.getMemberName()));
+
+        return removeDuplicates(membersToKick);
+    }
+
+    private List<String> calculateEldersToKick(List<EventHistory> eventHistoryOfElders) {
+        List<EventHistory> eldersThreshold = new ArrayList<>();
+        List<String> eldersToKick = new ArrayList<>();
+        eventHistoryOfElders.forEach(eventHistory -> {
+            if (thresholdPoints(eventHistory) || thresholdTickets(eventHistory)) {
+                eldersThreshold.add(eventHistory);
+            }
+        });
+        getDuplicates(eldersThreshold).forEach(eventHistory -> eldersToKick.add(eventHistory.getMemberName()));
+
+        return removeDuplicates(eldersToKick);
+    }
+
+    private boolean thresholdBeginner(EventHistory eventHistory) {
+        return (eventHistory.getDateJoinedTeam().isEqual(ONE_WEEK_AGO) ||
+                eventHistory.getDateJoinedTeam().isAfter(ONE_WEEK_AGO)) &&
+                (thresholdTickets(eventHistory) || thresholdPoints(eventHistory));
+    }
+
+    private boolean thresHoldElder(EventHistory eventHistory) {
+        return true;
+    }
+
+    private boolean thresholdTickets(EventHistory eventHistory) {
+        return eventHistory.getEventTicketsCollected() < 50 && eventHistory.getPointsCollected() == 0;
+    }
+
+    private boolean thresholdPoints(EventHistory eventHistory) {
+        return eventHistory.getEventTicketsCollected() == 0 && eventHistory.getPointsCollected() < 10;
+    }
+
+    private List<EventHistory> getDuplicates(List<EventHistory> eventHistories) {
+        return eventHistories.stream()
+                .collect(Collectors.groupingBy(EventHistory::getMemberId))
                 .entrySet().stream()
-                .filter(name -> name.getValue() > 1L)
-                .map(Map.Entry::getKey)
+                .filter(entry-> entry.getValue().size() > 1L)
+                .flatMap(entry-> entry.getValue().stream())
                 .collect(Collectors.toList());
     }
 
-    public List<Event> getLastTwoEvents() {
-        return eventRepository.getLastTwoEvents();
-    }
-
-    public List<EventHistory> getEventHistoryLastTwoEvents(List<Event> lastTwoEvents) {
-        List<EventHistory> eventHistories = eventHistoryRepository.getEventHistories();
-
-        eventHistories.removeIf(eventHistory ->
-                !eventHistory.getEventName().equalsIgnoreCase(lastTwoEvents.get(0).getName()) &&
-                        !eventHistory.getEventName().equalsIgnoreCase(lastTwoEvents.get(1).getName())
-        );
-
-        return eventHistories;
+    private List<String> removeDuplicates(List<String> memberNamesToKick) {
+        return memberNamesToKick.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .filter(name -> name.getValue() >= 1L)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/wolffsoft/phonedestroyer/CalculateTest.java
+++ src/test/java/com/wolffsoft/phonedestroyer/CalculateTest.java
@@ -1,11 +1,8 @@
 package com.wolffsoft.phonedestroyer;
 
 import com.wolffsoft.phonedestroyer.calculate.Calculate;
-import com.wolffsoft.phonedestroyer.model.Event;
 import com.wolffsoft.phonedestroyer.model.EventHistory;
-import com.wolffsoft.phonedestroyer.model.Member;
 import com.wolffsoft.phonedestroyer.repository.EventHistoryRepository;
-import com.wolffsoft.phonedestroyer.repository.EventRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,83 +14,52 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.wolffsoft.phonedestroyer.helperclass.model.EventHistoryTestObject.getTestEventHistories;
-import static com.wolffsoft.phonedestroyer.helperclass.model.MemberTestObject.getTestTeamMembers;
+import static com.wolffsoft.phonedestroyer.helperclass.model.EventTestObject.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalculateTest {
 
-    private static final String EVENT_TYPE_TICKETS = "Tickets";
-
-    @Mock
-    private EventRepository eventRepository;
-
     @Mock
     private EventHistoryRepository eventHistoryRepository;
 
     private Calculate calculate;
-    private Event event1;
-    private Event event2;
-    private List<Member> testMembers;
     private List<EventHistory> testEventHistories;
 
     @Before
     public void setup() {
-        testMembers = getTestTeamMembers();
         testEventHistories = getTestEventHistories();
-        event1 = Event.builder()
-                .id(1)
-                .name("Test Event 1")
-                .eventHasEnded(true)
-                .eventType(EVENT_TYPE_TICKETS)
-                .members(testMembers)
-                .build();
-
-        event2 = Event.builder()
-                .id(2)
-                .name("Test Event 2")
-                .eventHasEnded(true)
-                .eventType(EVENT_TYPE_TICKETS)
-                .members(testMembers)
-                .build();
-        calculate = new Calculate(eventRepository, eventHistoryRepository);
+        calculate = new Calculate(eventHistoryRepository);
     }
 
     @Test
     public void testCalculateMembersToBeKicked() {
-        List<Event> events = Stream.of(event2, event1).collect(Collectors.toList());
-
-        when(eventRepository.getLastTwoEvents()).thenReturn(events);
         when(eventHistoryRepository.getEventHistories()).thenReturn(testEventHistories);
 
-        List<String> memberNames = calculate.calculateMembersToBeKicked();
+        List<String> memberNames = calculate.membersToBeKicked();
 
-        assertThat(memberNames.size()).isEqualTo(4);
-
+        assertThat(memberNames.size()).isEqualTo(3);
     }
 
     @Test
-    public void testGetLastTwoEvents() {
-        List<Event> events = Stream.of(event2, event1).collect(Collectors.toList());
+    public void testGetEventHistoryFromLastFourEvents() {
+        when(eventHistoryRepository.getEventHistories()).thenReturn(testEventHistories);
 
-        when(eventRepository.getLastTwoEvents()).thenReturn(events);
+        List<EventHistory> returnedEventHistories = calculate.getEventHistoryLastFourEvents();
 
-        List<Event> returnedEvents = calculate.getLastTwoEvents();
-
-        assertThat(returnedEvents.size()).isEqualTo(2);
-        assertThat(returnedEvents.get(0).getId()).isEqualTo(event2.getId());
-        assertThat(returnedEvents.get(1).getId()).isEqualTo(event1.getId());
+        assertThat(returnedEventHistories.size()).isEqualTo(16);
     }
 
     @Test
-    public void testGetEventHistoryFromLastTwoEvents() {
-        List<Event> events = Stream.of(event1, event2).collect(Collectors.toList());
+    public void test() {
+        List<String> eventNames = Stream.of(
+                testEvent4.getName(),
+                testEvent3.getName(),
+                testEvent2.getName(),
+                testEvent1.getName()
+        ).collect(Collectors.toList());
 
-        when(eventHistoryRepository.getEventHistories()).thenReturn(testEventHistories);
-
-        List<EventHistory> returnedEventHistories = calculate.getEventHistoryLastTwoEvents(events);
-
-        assertThat(returnedEventHistories.size()).isEqualTo(12);
+        assertThat(eventNames.get(0)).isEqualTo(testEvent4.getName());
     }
 }
